b4bf761a18f5fab611e934e6b27668f86f9dd9e5
Ross Paffett <ross@rosspaffett.com>
2012-01-27 21:49:35 -0500

    Use Twitter Bootstrap layout

--- views/layout.php
+++ views/layout.php
@@ -3,8 +3,23 @@
 <head>
     <meta charset="utf-8">
     <title>Stratofortress &mdash; Organizer</title>
+    <link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/stylesheet.css" type="text/css" media="screen" charset="utf-8">
 </head>
 <body>
-    <?=$content;?>
+    <div class="topbar">
+        <div class="topbar-inner">
+            <div class="container">
+                <a class="brand" href="/~ross/organizer/">Organizer</a>
+                <ul class="nav">
+                    <li><a href="#">Overview</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    
+    <div class="container">
+        <?=$content;?>
+    </div>
 </body>
 </html>
