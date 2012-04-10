a2f704e7a43b0524c0ac959d07c7e32275450951
Ross Paffett <ross@rosspaffett.com>
2012-04-10 11:17:39 -0400

    Display original byte size in file size title

--- views/_entries.php
+++ views/_entries.php
@@ -3,7 +3,7 @@
 		<div class="file-name">
 			<strong class="file-basename" contenteditable></strong>
 			<small class="file-path" title="<?php echo($path); ?>"><?php echo($path); ?></small>
-			<small>(<span class="file-size"><?php echo($details["size"]); ?></span>)</small>
+			<small title="<?php echo($details["size"]); ?> bytes">(<span class="file-size"><?php echo($details["size"]); ?></span>)</small>
 		</div>
 		<div class="file-actions btn-group" data-toggle="buttons-radio">
 			<a class="btn btn-small ignore">Ignore</a>
