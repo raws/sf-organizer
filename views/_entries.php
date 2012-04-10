01ac632385719c75f1e3e49d9020ac871dffb320
Ross Paffett <ross@rosspaffett.com>
2012-04-09 23:55:11 -0400

    Place ignore button before delete button

--- views/_entries.php
+++ views/_entries.php
@@ -5,8 +5,8 @@
 			<small class="file-path" title="<?php echo($path); ?>"><?php echo($path); ?></small>
 		</div>
 		<div class="file-actions btn-group" data-toggle="buttons-radio">
-			<a class="btn btn-small delete">Delete</a>
 			<a class="btn btn-small ignore">Ignore</a>
+			<a class="btn btn-small delete">Delete</a>
 			<a class="btn btn-small movie">Movie</a>
 			<a class="btn btn-small tv">TV Show</a>
 		</div>
